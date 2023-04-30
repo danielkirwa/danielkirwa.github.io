6521a98d446aba0a8d7326f0896ff4cc10764e05
danielkirwa <kirwadan03@gmail.com>
2023-04-30 13:55:43 +0300

    Changed to js form php new sample project

--- inventory/js/config.js
+++ inventory/js/config.js
@@ -1,7 +1,4 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +15,5 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+   firebase.initializeApp(firebaseConfig);
+

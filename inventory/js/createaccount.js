e21f15099e9cc2e338034794d156f87693964495
danielkirwa <kirwadan03@gmail.com>
2023-05-02 23:27:44 +0300

    Select the data on firebase fixed

--- inventory/js/createaccount.js
+++ inventory/js/createaccount.js
@@ -160,3 +160,30 @@ function resetForm(){
   otherphone.value = "";
   email.value = "";
 }
+
+
+/// selected all data 
+
+ // Retrieve data from Firebase database
+      var table = document.getElementById("stafftable");
+      var ref = firebase.database().ref("Mystaff");
+      ref.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          var row = table.insertRow(-1);
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+          var cell5 = row.insertCell(4);
+          var cell6 = row.insertCell(5);
+          var cell7 = row.insertCell(6);
+          cell1.innerHTML = childData.FirstName;
+          cell2.innerHTML = childData.Email;
+          cell3.innerHTML = childData.Phone;
+          cell4.innerHTML = childData.Station;
+          cell5.innerHTML = childData.IDNumber;
+          cell6.innerHTML = childData.Role;
+          cell7.innerHTML = childData.DateAdded;
+        });
+      });

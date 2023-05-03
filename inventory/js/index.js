d3ae4a1945d9c9f8c8b471f22548b15a40ed729d
danielkirwa <kirwadan03@gmail.com>
2023-05-03 15:49:34 +0300

    eady for authentication part to be done

--- inventory/js/index.js
+++ inventory/js/index.js
@@ -1,3 +1,8 @@
+var success = "&#9989; Success";
+var failed = "&#10060; Failed";
+var warning = "&#10071; Warning";
+
+
  // Retrieve data from Firebase database
 var Bname,Baddress,Bphone,Bemail;
       var ref = firebase.database().ref("Mybusiness");
@@ -25,3 +30,55 @@ var Bname,Baddress,Bphone,Bemail;
           
         });
       });
+
+
+// authentication 
+let logintoaccount = document.getElementById('logintoaccount');
+let fillerror,fillerror1,fillerror2;
+
+logintoaccount.addEventListener('click', () =>{
+ let username = document.getElementById('username').value;
+let userpassword = document.getElementById('userpassword').value;
+
+ if (username == "" || userpassword == "" ) {
+   if (username == "") {
+      fillerror1 = " Enter Username";
+    }else{
+      fillerror1 = "";
+    }
+    if (userpassword == "") {
+      fillerror2 = "  Enter Password";
+    }else{
+      fillerror2 = "";
+    }
+
+        fillerror = 'Ensure that you  :  ' + fillerror1 +  fillerror2;
+     myAlert(warning, fillerror)
+
+  }else{
+      myAlert(success, "ready")
+  }
+
+})
+
+
+
+
+
+
+
+// alert
+function myAlert(title,message) {
+  var alertBox = document.getElementById("alertBox");
+  var alertTitle = document.getElementById("alertTitle");
+  var alertMessage = document.getElementById("alertMessage");
+  
+  alertTitle.innerHTML = title;
+  alertMessage.innerHTML = message;
+  alertBox.style.display = "block";
+}
+
+function hideAlert() {
+  var alertBox = document.getElementById("alertBox");
+  alertBox.style.display = "none";
+}

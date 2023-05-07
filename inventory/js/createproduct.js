d57a48f295a15d9afdd4b1958633a86fd0e85289
danielkirwa <kirwadan03@gmail.com>
2023-05-07 18:56:53 +0300

    Working on the product creation

--- inventory/js/createproduct.js
+++ inventory/js/createproduct.js
@@ -6,8 +6,47 @@ var today = new Date();
 var datetoday = today.toLocaleDateString();
 let usernamedisplay = document.getElementById('usernamedisplay');
 
-// write code here 
+let searchstock = document.getElementById('searchstock');
+let searchcode = document.getElementById('searchcode');
+// get display holders
+let lbstockname = document.getElementById('lbstockname');
+let lbstockfullname = document.getElementById('lbstockfullname');
+let lbstockcode = document.getElementById('lbstockcode');
+let availableunits = document.getElementById('availableunits');
+let createdby = document.getElementById('createdby');
+let unitsinstock = document.getElementById('unitsinstock');
+let lbclearby = document.getElementById('lbclearby');
 
+
+// write code here 
+searchstock.addEventListener('click', () =>{
+  let newsearchcode = searchcode.value;
+  
+  if (newsearchcode == "") {
+    myAlert(warning, "Enter code to search")
+  }else{
+  let searchnode = "Mystock/"+ newsearchcode ;
+  // get the stock 
+  var ref = firebase.database().ref(searchnode);
+  ref.once('value').then(function(snapshot) {
+  var childData = snapshot.val();
+  if (childData == null) {
+     myAlert(failed, "Search code not found")
+  }else{
+    lbstockname.innerHTML = childData.StockName;
+    lbstockfullname.innerHTML = childData.StockName;
+    lbstockcode.innerHTML = childData.Code;
+    availableunits.innerHTML = childData.UnitSale;
+    createdby.innerHTML = childData.Createby;
+    unitsinstock.value = childData.UnitSale;
+    lbclearby.innerHTML = childData.ClearBy;
+
+  }
+  
+
+});
+}
+})
 
 
 

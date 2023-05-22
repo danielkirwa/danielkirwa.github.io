1b37d3acafc6a23ea390a00008d18d1db32c6fe4
danielkirwa <kirwadan03@gmail.com>
2023-05-22 13:55:03 +0300

    Working on credit sales and report yet to do all update

---
+++ inventory/js/adjuststaff.js
@@ -0,0 +1,212 @@
+btnupdatestaff = document.getElementById('btnupdatestaff');
+searchstaffid = document.getElementById('searchstaffid');
+var success = "&#9989; Success";
+var failed = "&#10060; Failed";
+var warning = "&#10071; Warning";
+var today = new Date();
+var datetoday = today.toLocaleDateString();
+
+
+searchstaffid.addEventListener('click', () =>{
+  let txtsearchstaffid = document.getElementById('txtsearchstaffid').value;
+  // fields 
+  let firstname = document.getElementById('firstname');
+  let othername = document.getElementById('othername');
+  let phone = document.getElementById('phone');
+  let otherphone = document.getElementById('otherphone');
+  let idnumber = document.getElementById('idnumber');
+  let email = document.getElementById('email');
+  let doB = document.getElementById('dateofbirth');
+  var selectgender = document.getElementById("gender");
+  var selectstation = document.getElementById("station");
+  var selectrole = document.getElementById("role");
+
+  if (txtsearchstaffid == "") {
+    myAlert(warning, "Enter Staff IDNumber to search")
+  }else{
+
+   var searchfor = txtsearchstaffid;
+    let searchnode = "Mystaff/"+ searchfor;
+    //console.log(searchnode);
+  // get the stock 
+  var ref = firebase.database().ref(searchnode);
+  ref.once('value').then(function(snapshot) {
+  var childData = snapshot.val();
+  if (childData == null) {
+     myAlert(failed, "Search ID Number not found");
+  }else{
+     myAlert(success, "Staff found Update with care <br> <b style=\"color:crimson;\">Email and ID Number can not be Updated <br>contact Support to update</b> <br> <i> Click ok to update other staff details</i>");
+     firstname.value = childData.FirstName;
+     othername.value = childData.OtherName;
+     phone.value = childData.Phone;
+     otherphone = childData.OtherPhone;
+     idnumber.value = childData.IDNumber;
+     email.value = childData.Email;
+     selectgender.value = childData.Gender;
+     selectstation.value = childData.Station;
+     selectrole.value = childData.Role;
+     doB.value = childData.Dob;
+
+  }
+}); 
+
+  }
+
+})
+
+btnupdatestaff.addEventListener("click", () =>{
+let firstname = document.getElementById('firstname').value.toUpperCase();;
+let othername = document.getElementById('othername').value.toUpperCase();;
+let phone = document.getElementById('phone').value;
+let otherphone = document.getElementById('otherphone').value;
+let idnumber = document.getElementById('idnumber').value;
+let email = document.getElementById('email').value;
+let DoB = document.getElementById('dateofbirth').value;
+var selectgender = document.getElementById("gender");
+var selectedGenderOption = selectgender.value;
+var selectstation = document.getElementById("station");
+var selectedStationOption = selectstation.value;
+var selectrole = document.getElementById("role");
+var selectedRoleOption = selectrole.value;
+
+// validate data
+ 
+ if (firstname == "" || phone == "" || idnumber == "" || email == "" || DoB == "" || selectedGenderOption == "" || selectedStationOption == "" || selectedRoleOption == "") {
+ 	let fillerror,fillerror1,fillerror2,fillerror3,fillerror4,fillerror5,fillerror6,fillerror7,fillerror8;
+ 	 if (firstname == "") {
+      fillerror1 = "<br> Firstname";
+    }else{
+    	fillerror1 = "";
+    }
+    if (phone == "") {
+      fillerror2 = "<br> Phone";
+    }else{
+    	fillerror2 = "";
+    }
+    if (idnumber == "") {
+      fillerror3 = "<br> <b style=\"color:crimson;\">ID Number missing but will not be update just for confirmation</b> <br> <i>Id Number should be automaticaly filled search again staff</i>";
+    }else{
+    	fillerror3 = "";
+    }
+    if (email == "") {
+      fillerror4 = "<br> <b style=\"color:crimson;\">Email missing but will not be update just for confirmation</b> <br> <i>Email should be automaticaly filled search again staff</i>";
+    }else{
+    	fillerror4 = "";
+    }
+    if (DoB == "") {
+      fillerror5 = "<br> Date of birth";
+    }else{
+    	fillerror5 = "";
+    }
+    if (selectedGenderOption == "") {
+      fillerror6 = "<br> Select gender";
+    }else{
+    	fillerror6 = "";
+    }
+     if (selectedRoleOption == "") {
+      fillerror7 = "<br> Select Role";
+    }else{
+      fillerror7 = "";
+    }
+    if (selectedStationOption == "") {
+      fillerror8 = "<br> Select Station";
+    }else{
+      fillerror8 = "";
+    }
+
+
+
+
+  fillerror = 'Fill in the following :  ' + fillerror1 +  fillerror2 +  fillerror3 +  fillerror4 +  fillerror5  + fillerror6 + fillerror7 + fillerror8 ;
+ 	myAlert(warning, fillerror)
+
+ }else{
+ 	
+  // update data or write
+
+     firebase.database().ref("Mystaff/" + idnumber).update({
+
+       FirstName: firstname,
+      OtherName: othername,
+      Phone: phone,
+      OtherPhone: otherphone,
+      Gender: selectedGenderOption,
+      Role: selectedRoleOption,
+      Station: selectedStationOption,
+      Dob: DoB
+   
+      }).then(() => {
+   myAlert(success, "Staff details successfuly updated");
+   setTimeout(function(){
+    location.reload();
+     }, 3000); // 
+  })
+  .catch((error) => {
+     myAlert(failed, "Staff details not updated");
+  });
+
+
+ }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+function myAlert(title,message) {
+  var alertBox = document.getElementById("alertBox");
+  var alertTitle = document.getElementById("alertTitle");
+  var alertMessage = document.getElementById("alertMessage");
+  
+  alertTitle.innerHTML = title;
+  alertMessage.innerHTML = message;
+  alertBox.style.display = "block";
+}
+
+function hideAlert() {
+  var alertBox = document.getElementById("alertBox");
+  alertBox.style.display = "none";
+}
+
+
+function resetForm(){
+  firstname.value = "";
+  othername.value = "";
+  idnumber.value = "";
+  phone.value = "";
+  otherphone.value = "";
+  email.value = "";
+}
+
+
+
+
+
+         /// get business name and data 
+
+         // Retrieve the array from local storage and parse it back into an array
+        let storedBusiness = JSON.parse(localStorage.getItem('BusinessDetails'));
+
+          // Access a specific index of the array
+         lbbusinessname = document.getElementById('lbbusinessname');
+         lbbusinessname.innerHTML = storedBusiness[0];
+
+
+
+auth.onAuthStateChanged(function(user){
+      if(user){
+        var email = user.email;
+        //alert("Active user" + email);
+         usernamedisplay.innerHTML = email;
+      }else{
+        //alert("No Active user");
+        window.location.href='index.html';
+      }
+    })
---
+++ inventory/js/salecredit.js
@@ -0,0 +1,313 @@
+var success = "&#9989; Success";
+var failed = "&#10060; Failed";
+var warning = "&#10071; Warning";
+var today = new Date();
+var email;
+var datetoday = today.toLocaleDateString();
+let usernamedisplay = document.getElementById('usernamedisplay');
+const currentMonth = new Date().toLocaleString('default', { month: 'long' });
+const currentYear = new Date().getFullYear();
+
+//start of code
+
+let itemselected = document.getElementById('Itemselected');
+let newselectitem = document.getElementById('newselectitem');
+let newselectprice = document.getElementById('newselectprice');
+let newselectcode = document.getElementById('newselectcode');
+let txtnewselectitem = document.getElementById('txtnewselectitem');
+let txtnewselectprice = document.getElementById('txtnewselectprice');
+let txtnewselectedcode = document.getElementById('txtnewselectedcode');
+let itemcounter = document.getElementById('itemcounter');
+let btnaddtorecipt = document.getElementById('btnaddtorecipt');
+let snolabel = document.getElementById('snolabel');
+let priceholder = document.getElementById('priceholder');
+let tblpriceholder = document.getElementById('tblpriceholder');
+let tblgrandpriceholder = document.getElementById('tblgrandpriceholder');
+var count;
+var item 
+var totalamount;
+var removeditem = 0;
+var grandamount = 0;
+var recieptitems = 0
+var producttocodeupdate;
+var availableproductunittoupdate;
+var newavailableproductunittoupdate;
+
+
+// for local storage 
+
+
+let recieptitemsarray = [];
+let storedArray = [];
+
+itemselected.addEventListener("change", function(){ 
+item = Itemselected.options[Itemselected.selectedIndex].text;
+var price = Itemselected.options[Itemselected.selectedIndex].value;
+producttocodeupdate = Itemselected.options[Itemselected.selectedIndex].id;
+availableproductunittoupdate = Itemselected.options[Itemselected.selectedIndex].data;
+	newselectitem.innerHTML=  "Item. " +item;
+	newselectprice.innerHTML= "Ksh. " +price;
+	newselectcode.innerHTML = "Code. " + producttocodeupdate;
+	txtnewselectitem.value =  "" +item;
+	txtnewselectprice.value = "" +price;
+	txtnewselectedcode.value = "" + producttocodeupdate;
+	count = 1;
+	totalamount = price;
+	newavailableproductunittoupdate = availableproductunittoupdate - 1;
+	//console.log(newavailableproductunittoupdate);
+	//console.log(producttocodeupdate);
+
+});
+
+
+
+itemcounter.addEventListener("input", updateValue);
+
+function updateValue(e) {
+	var price = Itemselected.options[Itemselected.selectedIndex].value;
+	 count = e.target.value;
+	if (count < 1) {
+     itemcounter.value = 1;
+     newavailableproductunittoupdate = availableproductunittoupdate - count;
+     //console.log(newavailableproductunittoupdate);
+	}else{
+		newavailableproductunittoupdate = availableproductunittoupdate - count
+    totalamount = +count * +price;
+	newselectprice.innerHTML= "Ksh. " +totalamount;
+	txtnewselectitem.innerHTML =  "" +item;
+	txtnewselectprice.innerHTML = "" +price;
+	//console.log(newavailableproductunittoupdate);
+	}
+	
+}
+
+
+
+// retain table of the reciept data for refresh and picking up
+onreloadshowitems();
+function onreloadshowitems(argument) {
+	// body...
+	let storeditems = localStorage.getItem('curentreciept');
+	if (storeditems == null) {
+
+	}else{
+
+// Convert the array string back to an array using JSON.parse()
+ storedArray = JSON.parse(storeditems);
+
+// Get the table body element
+let tableBody = document.getElementById('recieptbody');
+
+// Clear any existing rows in the table
+tableBody.innerHTML = '';
+
+// Iterate over the array and create table rows
+storedArray.forEach(function(innerArray) {
+  let row = document.createElement('tr');
+  
+  innerArray.forEach(function(element) {
+    let cell = document.createElement('td');
+    cell.innerHTML = element;
+    row.appendChild(cell);
+
+  });
+ 
+  tableBody.appendChild(row);
+});
+ grandamount = storedArray.reduce((a, b) => a + +b[3],0);
+ recieptitems = storedArray.reduce((a, b) => a + +b[2],0);
+  //console.log(grandamount);
+  priceholder.innerHTML = grandamount;
+  tblpriceholder.innerHTML = grandamount;
+  tblgrandpriceholder.innerHTML = grandamount;
+  snolabel.innerHTML = recieptitems;
+}
+
+
+}
+
+
+
+
+
+// delete item
+function removeRow(button) {
+	let rowtoremoveformarray;
+	var removecount,remocevode;
+	var row = button.parentNode.parentNode;
+	 removeditem = row.getElementsByTagName("td")[3].textContent;
+	 removecount = row.getElementsByTagName("td")[2].textContent;
+	 removecode = row.getElementsByTagName("td")[1].textContent;
+	 grandamount = grandamount - +removeditem;
+	 priceholder.innerHTML = grandamount;
+	 tblpriceholder.innerHTML = grandamount;
+     tblgrandpriceholder.innerHTML = grandamount;
+     recieptitems = +recieptitems - removecount;
+  snolabel.innerHTML = recieptitems;
+   rowtoremoveformarray = row.rowIndex - 1;
+// data to delete
+ //console.log(removeditem);
+ //console.log(removecount);
+ //console.log(removecode)
+
+  recieptitemsarray = storedArray;
+   recieptitemsarray.splice(rowtoremoveformarray,1);
+   let storedreciept = JSON.stringify(recieptitemsarray);
+localStorage.setItem('curentreciept', storedreciept);
+//console.log(recieptitemsarray);
+			//remove row after subtraction
+/// update product count
+        firebase.database().ref('Myproduct/' + removecode + '/AvailableUnits').transaction(function(AvailableUnits) {
+  if (AvailableUnits === null) {
+    return 0; // If the value doesn't exist, set it to 1
+  } else {
+    return +AvailableUnits + +removecount; // Increment the value by 1
+  }
+});
+
+			row.parentNode.removeChild(row);
+			
+
+		}
+
+
+// print reciept
+
+printer.addEventListener('click', () => {
+  var divToPrint = document.getElementById("readyreciept").innerHTML;
+  var donotprint = document.querySelectorAll(".remove-btn");
+  var timestamp = Date.now();
+  // console.log(timestamp);
+  donotprint.forEach(function(element) {
+    element.style.visibility = "none";
+  });
+  // save sale and print reciept
+   //myAlert(success, "ready to save sale");
+  	 recieptitemsarray = storedArray;
+     // remove all the button create code form the reciept
+     for (let i = 0; i < recieptitemsarray.length; i++) {
+     	for(let j = 0; j < recieptitemsarray[i].length; j++){
+     		if (recieptitemsarray[i][j] === "<button class=\"remove-btn\" onclick=\"removeRow(this)\">X</button>") {
+     			recieptitemsarray[i].splice(j,1);
+     		}
+     	}
+      }
+
+  	 let storedreciepttodatabase = JSON.stringify(recieptitemsarray);
+     //console.log(storedreciepttodatabase);
+    /// update product count
+        firebase.database().ref('Mysale/' + timestamp).set(storedreciepttodatabase)
+  .then(function() {
+     // update monthly sales 
+
+    myAlert(success, "Sale completed ");
+    localStorage.removeItem("curentreciept");
+    //location.reload();
+  })
+  .catch(function(error) {
+     myAlert(failed, "Sale not completed ");
+  });
+   // update monthly sales 
+
+    let monthlysalenode = "Mymonthly/"+ currentMonth+currentYear ;
+        firebase.database().ref(monthlysalenode).update({
+
+       TotalSale: firebase.database.ServerValue.increment(grandamount)       
+   
+      }).then(() => {
+   
+  })
+  .catch((error) => {
+     myAlert(failed, "Cummulative not sales captured");
+  });
+
+  // update cashier sales 
+     
+     email = email.replace(/[@.]/g, "&");
+
+    let cashiersales = "Mycashiersales/"+ email ;
+        firebase.database().ref(cashiersales).update({
+
+       CashierTotalSale: firebase.database.ServerValue.increment(grandamount)       
+   
+      }).then(() => {
+       
+  })
+  .catch((error) => {
+     myAlert(failed, "Cummulative not cashier sales captured");
+  });
+
+
+
+   
+  var newWin = window.open('', 'Print-Window');
+  newWin.document.open();
+  newWin.document.write('<html><body onload="window.print()">' + divToPrint + '</body></html>');
+  newWin.document.close();
+  setTimeout(function() {
+    newWin.close();
+
+  }, 10);
+ 
+
+
+
+});
+
+
+
+
+
+
+
+
+// end off your code 
+// alert 
+
+function myAlert(title,message) {
+  var alertBox = document.getElementById("alertBox");
+  var alertTitle = document.getElementById("alertTitle");
+  var alertMessage = document.getElementById("alertMessage");
+  
+  alertTitle.innerHTML = title;
+  alertMessage.innerHTML = message;
+  alertBox.style.display = "block";
+}
+
+function hideAlert() {
+  var alertBox = document.getElementById("alertBox");
+  alertBox.style.display = "none";
+}
+
+/// get business name and data 
+
+         // Retrieve the array from local storage and parse it back into an array
+        let storedBusiness = JSON.parse(localStorage.getItem('BusinessDetails'));
+
+          // Access a specific index of the array
+         lbbusinessname = document.getElementById('lbbusinessname');
+         reciepttitle = document.getElementById('reciepttitle');
+         recieptaddress = document.getElementById('recieptaddress');
+         recieptphone = document.getElementById('recieptphone');
+         receiptdate = document.getElementById('receiptdate');
+         receiptname = document.getElementById('receiptname');
+         lbbusinessname.innerHTML = storedBusiness[0];
+         reciepttitle.innerHTML = storedBusiness[0];
+         recieptaddress.innerHTML = storedBusiness[1];
+         recieptphone.innerHTML = storedBusiness[2];
+         receiptdate.innerHTML = datetoday;
+
+
+
+
+auth.onAuthStateChanged(function(user){
+      if(user){
+        email = user.email;
+        //alert("Active user" + email);
+         usernamedisplay.innerHTML = email;
+         receiptname.innerHTML = email;
+      }else{
+        //alert("No Active user");
+        window.location.href='index.html';
+      }
+    })

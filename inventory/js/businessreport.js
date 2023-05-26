f1055fb2dd38a2709c3b81813d44ab0f2e328571
danielkirwa <kirwadan03@gmail.com>
2023-05-27 01:49:14 +0300

    Busisness analysis done basic more to be added

--- inventory/js/businessreport.js
+++ inventory/js/businessreport.js
@@ -6,6 +6,13 @@ const currentDate = new Date();
 const previousMonthIndex = currentDate.getMonth() - 1;
 const previousMonthName = months[previousMonthIndex];
 
+let tbltotalsale = document.getElementById('tbltotalsale');
+let tbltotalcredit = document.getElementById('tbltotalcredit');
+
+let averagecredit = 0;
+let totalcredit = 0;
+let averageSales = 0;
+let totalSales = 0;
 /* =============================================================*/
   // cash sale select
 // Fetch sales data for a specific month
@@ -40,12 +47,13 @@ async function getAllMonthsSales() {
     salesData.push(monthSales);
   }
   
-  let totalSales = 0;
+
   for (let i = 0; i < salesData.length; i++) {
     totalSales += salesData[i];
   }
-  const averageSales = totalSales / salesData.length;
-  console.log("Sales Total :", totalSales); 
+  averageSales = totalSales / salesData.length;
+  tbltotalsale.innerHTML = "Sales Total : <br>" + totalSales;
+
   console.log("Avarage:", averageSales.toFixed(2)); 
   return salesData;
 }
@@ -111,13 +119,13 @@ async function getAllMonthsCredit() {
     creditData.push(monthCredit);
   }
   
-  let totalcredit = 0;
+  
   for (let i = 0; i < creditData.length; i++) {
     totalcredit += creditData[i];
   }
-  const averagecredit = totalcredit / creditData.length;
-  console.log("Sales Total :", totalcredit); 
-  console.log("Avarage:", averageSales.toFixed(2)); 
+  averagecredit = totalcredit / creditData.length;
+  tbltotalcredit.innerHTML = "Total Credit : <br>" + totalcredit;
+  console.log("Avarage:", averagecredit.toFixed(2)); 
   return creditData;
 }
 
@@ -146,6 +154,83 @@ function createBarGraphforCredit(creditData) {
 }
 
 
+/* =============================================================*/
+
+// business numbers
+function businessNumbesr() {
+	// body...
+	let tbltotalsalepercent = document.getElementById('tbltotalsalepercent');
+	let tbltotalcreditpercent = document.getElementById('tbltotalcreditpercent');
+	let tblgrandtotal = document.getElementById('tblgrandtotal');
+	let tblgrandtotalpercent = document.getElementById('tblgrandtotalpercent');
+	let tbltotalsaleav = document.getElementById('tbltotalsaleav');
+	let tbltotalcreditav = document.getElementById('tbltotalcreditav');
+
+	let newtotalsale = totalSales;
+	let newtaotalcredit = totalcredit;
+	let avnewtotalsale = averageSales.toFixed(2);
+	let avnewtotalcredit = averagecredit.toFixed(2);
+	let newgrandtotal = newtaotalcredit + newtotalsale;
+	let cashpercent = (newtotalsale / newgrandtotal) * 100 
+	cashpercent = cashpercent.toFixed(2);
+	let creditpercent = (newtaotalcredit / newgrandtotal) * 100
+	creditpercent = creditpercent.toFixed(2);
+
+	// show calculations
+	tbltotalsalepercent.innerHTML = cashpercent +  " %";
+	tbltotalcreditpercent.innerHTML =  creditpercent + " %";
+	tblgrandtotal.innerHTML = "Grand Total : <br>" + newgrandtotal;
+	tblgrandtotalpercent.innerHTML = +cashpercent + +creditpercent + " %";
+	tbltotalsaleav.innerHTML = "Avarage : <br>" + avnewtotalsale;
+	tbltotalcreditav.innerHTML = "Avarage : <br>" + avnewtotalcredit;
+}
+
+ function linegraph(salesData,creditData) {
+ 	// body...
+
+    // Labels for the x-axis (e.g., months, dates, etc.)
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    // Create a new chart instance
+    const ctx = document.getElementById('lingraphcomparison').getContext('2d');
+    const lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Cash Sales',
+            data: salesData,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Set the background color of the line area
+            borderColor: 'rgba(75, 192, 192, 1)', // Set the border color of the line
+            borderWidth: 1, // Set the border width
+            fill: 'start', // Fill the area under the line
+          },
+          {
+            label: 'Credit Sales',
+            data: creditData,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            fill: 'start',
+          },
+        ],
+      },
+      options: {
+        responsive: true, // Make the chart responsive
+        maintainAspectRatio: false, // Prevent the chart from being stretched
+        scales: {
+          y: {
+            beginAtZero: true, // Start the y-axis from zero
+          },
+        },
+      },
+    });
+ }
+
+
+linegraph();
+
 
 
 // call functions in order
@@ -153,16 +238,19 @@ getAllMonthsSales()
   .then((salesData) => {
     createBarGraph(salesData);
     //plotSalesTrend(salesData);
-  })
+      
+  getAllMonthsCredit()
+  .then((creditData) => {
+    createBarGraphforCredit(creditData);
+     businessNumbesr();
+     linegraph(salesData,creditData);
+    
+    })
   .catch((error) => {
     console.error("Error:", error);
   });
 
-  getAllMonthsCredit()
-  .then((creditData) => {
-    createBarGraphforCredit(creditData);
-    
-    })
+  })
   .catch((error) => {
     console.error("Error:", error);
   });

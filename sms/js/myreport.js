7430537e58c1dd6df5266fd2a41ec409b7971a25
danielkirwa <kirwadan03@gmail.com>
2023-11-28 22:37:43 +0300

    Agent added

--- sms/js/myreport.js
+++ sms/js/myreport.js
@@ -143,11 +143,12 @@ function updateStatus(timestamp) {
   // Update the status to 1
   firebase.database().ref(databasePath).update({ Status: 1 })
     .then(function() {
-      myAlertRefresh(success,"Cleared ")
+      myAlertRefresh(success,"Cleared ");
       // You may want to refresh the data display or perform other actions
     })
     .catch(function(error) {
-      myAlert(warning,"Failed to clear");
+      console.error("Error updating status:", error);
+      myAlert(warning, "Failed to clear");
     });
 }
 
@@ -194,6 +195,21 @@ function myAlert(title,message) {
 function hideAlert() {
   var alertBox = document.getElementById("alertBox");
   alertBox.style.display = "none";
+}
+function myAlertRefresh(title,message) {
+  var alertBox = document.getElementById("alertBoxRefresh");
+  var alertTitle = document.getElementById("alertTitle1");
+  var alertMessage = document.getElementById("alertMessage1");
+  
+  alertTitle.innerHTML = title;
+  alertMessage.innerHTML = message;
+  alertBox.style.display = "block";
+}
+
+function hideAlertRefresh() {
+  var alertBox = document.getElementById("alertBoxRefresh");
+  alertBox.style.display = "none";
+  location.reload();
 }
 
 /// get business name and data 

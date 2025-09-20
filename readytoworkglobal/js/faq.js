b8c1f427d234ac667e7c81c8a3d3c260173920f1
danielkirwa <kirwadan03@gmail.com>
2025-09-20 12:35:41 +0300

    service added culture done

---
+++ readytoworkglobal/js/faq.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.parentElement;
+    const answer = item.querySelector('.faq-answer');
+
+    item.classList.toggle('active');
+
+    if (item.classList.contains('active')) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = null;
+    }
+  });
+});

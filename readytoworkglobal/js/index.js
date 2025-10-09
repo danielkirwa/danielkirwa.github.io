2df3d282b0cb5714e3c2c4892f3592fcd61622d3
danielkirwa <kirwadan03@gmail.com>
2025-10-09 18:10:28 +0300

    logo added and made a clickable link

--- readytoworkglobal/js/index.js
+++ readytoworkglobal/js/index.js
@@ -1,24 +1,3 @@
-const hero = document.getElementById('hero');
-const images = [
-  'url("assets/bg1.png")',
-  'url("assets/bg2.png")',
-  'url("assets/bg3.png")',
-  'url("assets/bg4.jpg")',
-  'url("assets/bg5.jpg")'
-];
-
-let index = 0;
-function rotateHeroBackground() {
-  hero.style.backgroundImage = images[index];
-  index = (index + 1) % images.length;
-}
-rotateHeroBackground(); // Initial load
-setInterval(rotateHeroBackground, 5000); // Rotate every 5 seconds
-
-document.querySelector('.action-btn').addEventListener('click', () => {
-  alert('Welcome to GlobalBridge Solutions! Let’s get you started.');
-});
-
 
 
 const observer1 = new IntersectionObserver((entries) => {

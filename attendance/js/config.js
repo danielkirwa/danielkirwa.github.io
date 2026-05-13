952c47b535615ccef5118420dbdf9df1ecd14aa0
danielkirwa <kirwadan03@gmail.com>
2026-05-13 17:22:50 +0300

    dashboard links added

--- attendance/js/config.js
+++ attendance/js/config.js
@@ -15,3 +15,12 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth()
 
 console.log ('connected to firebase')
+
+function  logout() {
+  // body...
+  firebase.auth().signOut().then(function (){
+    window.location.href = "index.html"
+  }).catch((error) =>{
+    alert("Error while you try to logout")
+  })
+}
